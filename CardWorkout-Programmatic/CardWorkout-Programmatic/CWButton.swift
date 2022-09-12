b875c8c64d85c382d55d11c9466aafef6b0021e7
Maxim Bekmetov <mbekmetov@ibs.ru>
2022-09-12 14:51:36 +0300

    lesson 43

--- CardWorkout-Programmatic/CardWorkout-Programmatic/CWButton.swift
+++ CardWorkout-Programmatic/CardWorkout-Programmatic/CWButton.swift
@@ -11,26 +11,25 @@ class CWButton: UIButton {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        configure()
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    init(backgroundColor: UIColor, title: String) {
+    init(color: UIColor, title: String, systemImageName: String) {
         super.init(frame: .zero)
-        self.backgroundColor = backgroundColor
-        setTitle(title, for: .normal)
-        configure()
-    }
 
+        configuration = .tinted()
+        configuration?.title = title
+        configuration?.baseForegroundColor = color
+        configuration?.baseBackgroundColor = color
+        configuration?.cornerStyle = .medium
 
-    func configure() {
-        layer.cornerRadius = 8
-        titleLabel?.font = .systemFont(ofSize: 19, weight: .bold)
-        setTitleColor(.white, for: .normal)
+        configuration?.image = UIImage(systemName: systemImageName)
+        configuration?.imagePadding = 6
+        configuration?.imagePlacement = .trailing
+
         translatesAutoresizingMaskIntoConstraints = false
     }
-
 }
--- CardWorkout-Programmatic/CardWorkout-Programmatic/CardSelectionVC.swift
+++ CardWorkout-Programmatic/CardWorkout-Programmatic/CardSelectionVC.swift
@@ -10,15 +10,36 @@ import UIKit
 class CardSelectionVC: UIViewController {
 
     let cardImageView = UIImageView()
-    let stopButton = CWButton(backgroundColor: .systemRed, title: "Stop!")
-    let resetButton = CWButton(backgroundColor: .systemGreen, title: "Reset")
-    let rulesButton = CWButton(backgroundColor: .systemBlue, title: "Rules")
+    let stopButton = CWButton(color: .systemRed, title: "Stop!", systemImageName: "stop.circle")
+    let resetButton = CWButton(color: .systemGreen, title: "Reset", systemImageName: "arrow.clockwise.circle")
+    let rulesButton = CWButton(color: .systemBlue, title: "Rules", systemImageName: "list.bullet")
+
+    var cards = CardDeck.allValues
+    var timer: Timer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
         configureUI()
+        startTimer()
+    }
+
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(showRandomCard), userInfo: nil, repeats: true)
+    }
+
+    @objc func stopTimer() {
+        timer.invalidate()
+    }
+
+    @objc func resetTimer() {
+        stopTimer()
+        startTimer()
+    }
+
+    @objc func showRandomCard() {
+        cardImageView.image = cards.randomElement()
     }
 
     func configureUI() {
@@ -43,6 +64,7 @@ class CardSelectionVC: UIViewController {
 
     func configureStopButton() {
         view.addSubview(stopButton)
+        stopButton.addTarget(self, action: #selector(stopTimer), for: .touchUpInside)
 
         NSLayoutConstraint.activate([
             stopButton.widthAnchor.constraint(equalToConstant: 260),
@@ -54,6 +76,8 @@ class CardSelectionVC: UIViewController {
 
     func configureResetButton() {
         view.addSubview(resetButton)
+        resetButton.addTarget(self, action: #selector(resetTimer), for: .touchUpInside)
+
 
         NSLayoutConstraint.activate([
             resetButton.widthAnchor.constraint(equalToConstant: 115),
@@ -65,6 +89,7 @@ class CardSelectionVC: UIViewController {
 
     func configureRulesButton() {
         view.addSubview(rulesButton)
+        rulesButton.addTarget(self, action: #selector(presentRulesVC), for: .touchUpInside)
 
         NSLayoutConstraint.activate([
             rulesButton.widthAnchor.constraint(equalToConstant: 115),
@@ -72,6 +97,10 @@ class CardSelectionVC: UIViewController {
             rulesButton.trailingAnchor.constraint(equalTo: stopButton.trailingAnchor),
             rulesButton.topAnchor.constraint(equalTo: stopButton.bottomAnchor, constant: 20)
         ])
+    }
+
+    @objc func presentRulesVC() {
+        present(RulesVC(), animated: true)
     }
 
 }
